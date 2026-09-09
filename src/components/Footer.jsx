25e58c0dd1ed4b2cdc01ee52d67b6237daa5658f
Yacine2004-lab <yacinesall262@gmail.com>
2026-09-09 14:13:23 +0000

    Fix Facebook URL to BayeNiass

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const socialLinks = [
   },
   {
     name: 'Facebook',
-    href: 'https://www.facebook.com/bayeniass.services',
+    href: 'https://www.facebook.com/BayeNiass',
     title: 'Suivez-nous sur Facebook',
     Icon: FacebookIcon,
     hoverClass: 'hover:border-[#1877F2]/60 hover:bg-[#1877F2]/15 hover:text-[#1877F2]',
